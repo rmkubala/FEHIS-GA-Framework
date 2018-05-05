1411a45837f02af078fb10bd6ba5dc611f41a866
Radoslaw Kubala <radoslaw.kubala@gmail.com>
2018-05-05 09:21:55 +0200

    Implemented CountingStopCondition.kt

--- src/pl/polsl/ga/impl/CountingStopCondition.kt
+++ src/pl/polsl/ga/impl/CountingStopCondition.kt
@@ -3,8 +3,8 @@ package pl.polsl.ga.impl
 import pl.polsl.ga.general.Individual
 import pl.polsl.ga.general.StopCondition
 
-class CountingStopCondition : StopCondition() {
+class CountingStopCondition(private val generationCount: Int) : StopCondition() {
     override fun <T : Individual<*>> shouldContinue(population: ArrayList<T>, generation: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return generation < generationCount
     }
 }
